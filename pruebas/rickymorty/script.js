5cc1670473e920f7da90e94d497af5312b8047d0
Inakipf <inakipf@gmail.com>
2022-06-15 17:44:18 +0200

    funciones

--- pruebas/rickymorty/script.js
+++ pruebas/rickymorty/script.js
@@ -1,89 +1,68 @@
 "use strict";
 
-const urlRick = `https://rickandmortyapi.com/api/character/?page=`;
-const urlRickPages = "https://rickandmortyapi.com/api/character/";
+const URL_RICK = `https://rickandmortyapi.com/api/character/?page=`;
+const URL_RICK_PAGES = "https://rickandmortyapi.com/api/character/";
+let paginas = getRickPages(URL_RICK_PAGES);
+let paginaCargada = 1;
 
+//& Funcion que devuelve cuantas paginas tiene en total
 async function getRickPages(urlApi){
 	try {
-		let paginas;
 		let response = await fetch(urlApi);
 		let datos = await response.json();
-		return paginas = datos.info.pages;
-		
+		return datos.info.pages;
 	} catch (error) {
 		console.log(error)
 	}
 }
 
+//& devuelve array con 20 personajes 
 async function getRick(urlApi,page){
 	let response = await fetch(urlApi+page);
 	let datos = await response.json();
 	return datos.results	
 };
 
+function pintaTarjeta(res,index){
+	return `<article class="card" id="card"> 
+			 	<div class="card-body">
+					<img src="${res[index].image}" alt="" class="card-body-img">
+					<h1 class="card-body-title">
+						${res[index].name}
+					</h1>
+					<p class="card-body-text">${res[index].species}</p>
+				</div>
+		
+				<div class="card-footer">
+					<div class="card-footer-social">
+						<h3>${res[index].origin.name}</h3>
+						<p>Origin</p>
+					</div>
+	
+					<div class="card-footer-social">
+						<h3>${res[index].status}</h3>
+						<p>Status</p>
+					</div>
+	
+					<div class="card-footer-social">
+						<h3>${res[index].gender}</h3>
+						<p>Gender</p>
+					</div>
+				</div>
+			</article>`	
+}
 
-let paginas = getRickPages(urlRickPages);
-let paginaCargada = 1;
 
-getRick(urlRick,1).then((res) =>{
+getRick(URL_RICK,1).then((res) =>{										//& Cuando recibe la promesa resuelta
 	console.log(res);
 	let titulo = document.querySelector(".flex");
 
+	
 	for (let index = 0; index < res.length; index++) {
-		if (index === 1) {
-			titulo.innerHTML = `<article class="card" id="card"> 
-										 	<div class="card-body">
-												<img src="${res[index].image}" alt="Imagen de Inaki" class="card-body-img">
-												<h1 class="card-body-title">
-												${res[index].name}
-												</h1>
-												<p class="card-body-text">${res[index].species}</p>
-											</div>
-	
-											<div class="card-footer">
-												<div class="card-footer-social">
-														<h3>${res[index].origin.name}</h3>
-														<p>Origin</p>
-												</div>
-	
-												<div class="card-footer-social">
-													<h3>${res[index].status}</h3>
-													<p>Status</p>
-												</div>
-	
-												<div class="card-footer-social">
-													<h3>${res[index].gender}</h3>
-													<p>Gender</p>
-												</div>
-											</div>
-										</article>`	
+		if (index === 0) {
+			titulo.innerHTML = pintaTarjeta(res,index);
 		} else {			
-			titulo.innerHTML += `<article class="card" id="card"> 
-									<div class="card-body">
-										<img src="${res[index].image}" alt="Imagen de Inaki" class="card-body-img">
-										<h1 class="card-body-title">
-											${res[index].name}
-										</h1>
-										<p class="card-body-text">${res[index].species}</p>
-									</div>
-						
-									<div class="card-footer">
-										<div class="card-footer-social">
-											<h3>${res[index].origin.name}</h3>
-											<p>Origin</p>
-										</div>
-		
-										<div class="card-footer-social">
-											<h3>${res[index].status}</h3>
-											<p>Status</p>
-										</div>
-								
-										<div class="card-footer-social">
-											<h3>${res[index].gender}</h3>
-											<p>Gender</p>
-										</div>
-									</div>
-								</article>`
+			titulo.innerHTML +=  pintaTarjeta(res,index);
 	}
 	}
 	
@@ -91,43 +70,14 @@ getRick(urlRick,1).then((res) =>{
 
 window.addEventListener('scroll', () => {
 	
-     if (window.scrollY >= (document.documentElement.scrollHeight - window.innerHeight)-100) {
+    if (window.scrollY >= (document.documentElement.scrollHeight - window.innerHeight)-100) {
 		paginaCargada++
-		getRick(urlRick,paginaCargada).then((res) =>{
+		getRick(URL_RICK,paginaCargada).then((res) =>{
 			console.log(res);
-			let titulo = document.querySelector(".flex");
-		
+			let titulo = document.querySelector(".flex");	
 			for (let index = 0; index < res.length; index++) {
-			
-					titulo.innerHTML += `<article class="card" id="card"> 
-											<div class="card-body">
-												<img src="${res[index].image}" alt="Imagen de Inaki" class="card-body-img">
-												<h1 class="card-body-title">
-													${res[index].name}
-												</h1>
-												<p class="card-body-text">${res[index].species}</p>
-											</div>
-								
-											<div class="card-footer">
-												<div class="card-footer-social">
-													<h3>${res[index].origin.name}</h3>
-													<p>Origin</p>
-												</div>
-				
-												<div class="card-footer-social">
-													<h3>${res[index].status}</h3>
-													<p>Status</p>
-												</div>
-										
-												<div class="card-footer-social">
-													<h3>${res[index].gender}</h3>
-													<p>Gender</p>
-												</div>
-											</div>
-										</article>`
-			
+				titulo.innerHTML +=  pintaTarjeta(res,index);
 			}
-			
 		});	
 	}
 });
