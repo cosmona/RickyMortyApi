e5f48541b2ea1f4a803a1967707e7d614122103d
Inakipf <inakipf@gmail.com>
2022-06-16 12:37:14 +0200

    volteo tarjetas

--- pruebas/rickymorty/script.js
+++ pruebas/rickymorty/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
 const URL_RICK = `https://rickandmortyapi.com/api/character/?page=`;
-const URL_RICK_PAGES = "https://rickandmortyapi.com/api/character/";
+const URL_RICK_PAGES = `https://rickandmortyapi.com/api/character/`;
+
 let paginas = getRickPages(URL_RICK_PAGES);
 let paginaCargada = 1;
 
@@ -23,41 +24,44 @@ async function getRick(urlApi,page){
 	return datos.results	
 };
 
+//& devuelve una tarjeta rellena con los datos
 function pintaTarjeta(res,index){
 	return `<article class="card" id="card"> 
-			 	<div class="card-body">
-					<img src="${res[index].image}" alt="" class="card-body-img">
-					<h1 class="card-body-title">
-						${res[index].name}
-					</h1>
-					<p class="card-body-text">${res[index].species}</p>
-				</div>
+				<div class="cardFront">
+					<div class="card-body">
+						<img src="${res[index].image}" alt="" class="card-body-img">
+						<h1 class="card-body-title">
+							${res[index].name}
+						</h1>
+						<p class="card-body-text">${res[index].species}</p>
+					</div>
+			
+					<div class="card-footer">
+						<div class="card-footer-social">
+							<h3>${res[index].origin.name}</h3>
+							<p>Origin</p>
+						</div>
 		
-				<div class="card-footer">
-					<div class="card-footer-social">
-						<h3>${res[index].origin.name}</h3>
-						<p>Origin</p>
+						<div class="card-footer-social">
+							<h3>${res[index].status}</h3>
+							<p>Status</p>
+						</div>
+		
+						<div class="card-footer-social">
+							<h3>${res[index].gender}</h3>
+							<p>Gender</p>
+						</div>
 					</div>
-	
-					<div class="card-footer-social">
-						<h3>${res[index].status}</h3>
-						<p>Status</p>
-					</div>
-	
-					<div class="card-footer-social">
-						<h3>${res[index].gender}</h3>
-						<p>Gender</p>
-					</div>
+				</div>
+				<div class="cardBack">
+				<img src="./images/Google-Maps.png" alt="">${res[index].url}</img>
 				</div>
 			</article>`	
 }
 
-
-getRick(URL_RICK,1).then((res) =>{										//& Cuando recibe la promesa resuelta
+getRick(URL_RICK,1).then((res) =>{																//* Cuando recibe la promesa resuelta
 	console.log(res);
 	let titulo = document.querySelector(".flex");
-
-	
 	for (let index = 0; index < res.length; index++) {
 		if (index === 0) {
 			titulo.innerHTML = pintaTarjeta(res,index);
@@ -68,12 +72,10 @@ getRick(URL_RICK,1).then((res) =>{										//& Cuando recibe la promesa resuelt
 	
 });	
 
-window.addEventListener('scroll', () => {
-	
+window.addEventListener('scroll', () => {														//* Cuando baja el scroll	
     if (window.scrollY >= (document.documentElement.scrollHeight - window.innerHeight)-100) {
 		paginaCargada++
 		getRick(URL_RICK,paginaCargada).then((res) =>{
-			console.log(res);
 			let titulo = document.querySelector(".flex");	
 			for (let index = 0; index < res.length; index++) {
 				titulo.innerHTML +=  pintaTarjeta(res,index);
