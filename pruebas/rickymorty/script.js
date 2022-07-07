d3afcb091e1dcaa08bb80c58a718ac21d707566b
Inaki Perez <inakipf@gmail.com>
2022-07-07 20:03:41 +0200

    limpieza y comentado

--- pruebas/rickymorty/script.js
+++ pruebas/rickymorty/script.js
@@ -25,6 +25,8 @@ async function getRick(urlApi,page){
 //& devuelve una tarjeta rellena con los datos
 async function pintaTarjeta(res,index){
 	let episodios;
+
+	//* monto un html de cada tarjeta
 	let pizarra = `<article class="card" id="card${res[index].id}"> 
 						<section class="imagenTitulo">
 							<img src="${res[index].image}" alt="" class="card-body-img">
@@ -57,8 +59,6 @@ async function pintaTarjeta(res,index){
 	});
 	
 	pizarra += `</div></section>`;
-	
-	//* pinta una tarjeta del array res en la posicion index
 	return pizarra;
 };
 
@@ -66,25 +66,26 @@ async function pintaTarjeta(res,index){
 async function main(){
 	const URL_RICK = `https://rickandmortyapi.com/api/character/?page=`;
 	const URL_RICK_PAGES = `https://rickandmortyapi.com/api/character/`;
+	let paginaCargada = 1;
+	let scrollY;
 	
+	//~ pesco donde van a ir las tarjetas
 	const TITULO = document.querySelector(".flex"); //~ DOM
 	
+	//* obtengo el numero de paginas
 	const PAGINAS = await getRickPages(URL_RICK_PAGES);
 	
-	let paginaCargada = 1;
-	
-	//* numero total de paginas en la api
+	//* hago una consulta de los primeros 20 personajes (1ª pagina)
 	let res = await getRick(URL_RICK,1)
 	
-	let scrollY;
 		
 	//^ Handle funcion cuando se hace click en una tarjeta
-	let handleFuction = function () {
+	let handleFuction = function () { 																						//! cuando una funcion callback es tan grande, no es mejor definirla como funcion normal?
 		
 		let pescador = document.querySelector(`.datosTarjeta`); //~ DOM
-		console.log(this)
-		//* Si ya esta abierto lo cierra
-		if (this.classList.contains('Abre')) {
+
+		//* Si ya esta abierto lo cierra																					
+		if (this.classList.contains('Abre')) {																				//! uso de this por problemas en el eventlistener.
 			this.removeAttribute( 'style');
 			this.classList.remove('Abre');
 			pescador.classList.remove("aparece");
@@ -92,22 +93,22 @@ async function main(){
 			//* hace scroll automatico hacia donde estaba la tarjeta
 			window.scrollTo({top: scrollY, behavior: 'smooth'});
 
-		} else { //* se quiere abrir una tarjeta
+		} else { //* abrir una tarjeta
 			
 			//* posicion actual del scroll
 			scrollY = window.scrollY;
 			
-			//* recorre todas las .cards y les borra la clase Abre y el style para verrar si habia alguna abierta
+			//* recorre todas las .cards y les borra la clase Abre y el style para cerrar si habia alguna abierta
 			document.querySelectorAll(".card").forEach((tarjeta) => {
 				tarjeta.classList.remove('Abre');
 				tarjeta.removeAttribute('style');
 			});
 			
 			//* se guarda la posicion actual de la tarjeta clickada
-			var startTop = this.offsetTop;
-			var startLeft = this.offsetLeft;
+			let startTop = this.offsetTop;
+			let startLeft = this.offsetLeft;
 
-			//* le añade la clase Abre pone la posicion absolute y la posiciona donde estaba
+			//* le añade la clase Abre,pone la posicion absolute y la posiciona donde estaba
 			this.classList.add('Abre');
 			this.style.position = 'absolute';
 			this.style.left = `${startLeft}px`;
@@ -130,24 +131,33 @@ async function main(){
 		};
 
 		//* añade un escuchador (handleFuction) click por cada episodio
-		document.querySelectorAll(".capitulos").forEach((tarjeta) => tarjeta.addEventListener("click",async (event)=>{
+		document.querySelectorAll(".capitulos").forEach((tarjeta) => tarjeta.addEventListener("click",async (event)=>{ //~DOM
 			try {
 				event.stopPropagation();
+				//* consulta a la api el episodio clickado
 				let response = await fetch(`https://rickandmortyapi.com/api/episode/${event.target.innerHTML}`);
 				let datos = await response.json();
+
+				//* crea una nueva section, le añade la clase personajesCard y crea la section characters dentro
 				const newSection = document.createElement("section");
 				newSection.classList.add("personajesCard")
 				newSection.innerHTML += '<section class="characters">';
-				let pescador = document.querySelector(`#${this.id} .datosTarjeta`);
-				let pescador2 = document.querySelector(".personajesCard")
+
+				//* selecciona la section datosTarjeta 
+				let pescador = document.querySelector(`#${this.id} .datosTarjeta`); //~DOM
+
+				//* selecciona la section personajesCard y si existe lo borra
+				let pescador2 = document.querySelector(".personajesCard"); //~DOM
 				if (pescador2 != null){
 					pescador2.remove();
 				}
+
+				//* por cada personaje del capitulo
 				for (let index = 0; index < datos.characters.length; index++) {
 					let cadena = datos.characters[index].split("/");
 					let numPersonaje = cadena[cadena.length-1];
 
-					//* Agregamos un texto al nuevo " ".
+					//* Agregamos un texto al nuevo "newSection".
 					newSection.innerHTML += `<img src="https://rickandmortyapi.com/api/character/avatar/${numPersonaje}.jpeg" alt="" class="card-body-img">`;
 
 					//* Incorporamos el "" como último hijo del "".
@@ -159,8 +169,8 @@ async function main(){
 				console.error(error)
 			}
 		}));
-	}; //^ fin Handle funcion
-
+	}; 
+	//^ fin Handle funcion
 
 	//* pinta los primeros 20 personajes
 	for (let index = 0; index < res.length; index++) {
@@ -168,34 +178,34 @@ async function main(){
 		else TITULO.innerHTML +=  await pintaTarjeta(res,index);
 	}
 
-	//* añade un escuchador (handleFuction) click por cada tarjeta
+	//* añade un escuchador (handleFuction) click por cada tarjeta para las primera 20 tarjetas
 	document.querySelectorAll(".card").forEach((tarjeta) => tarjeta.addEventListener("click",handleFuction));
 	 
 	
 	//* Cuando baja el scroll genera 20 tarjetas mas
-	window.addEventListener('scroll', async () => {	
+	window.addEventListener('scroll', async function () { 																				//! funcion de flecha asyncrona?
 
-		//* si hay alguna tarjeta abierta no genera mas al bajar
-		let abierto = document.querySelectorAll(".Abre").length;
-		if (!abierto >0 ){
+			//* si hay alguna tarjeta abierta no genera mas al bajar
+			let abierto = document.querySelectorAll(".Abre").length; //~ DOM
+			if (!abierto > 0) {
 
-			//* si el scroll llega al final de la pagina comprueba si hay mas de la api
-			if (window.scrollY >= (document.documentElement.scrollHeight - window.innerHeight)-200) {
-				paginaCargada++
-				if (paginaCargada <= PAGINAS) {
-					res = await getRick(URL_RICK,paginaCargada);
-					for (let index = 0; index < res.length; index++) {
-						TITULO.innerHTML += await pintaTarjeta(res,index);
+				//* si el scroll llega al final de la pagina comprueba si hay mas de la api
+				if (window.scrollY >= (document.documentElement.scrollHeight - window.innerHeight) - 200) {
+					paginaCargada++;
+					if (paginaCargada <= PAGINAS) {
+						//* hace una consulta pasandole la pagina nueva
+						res = await getRick(URL_RICK, paginaCargada);
+						for (let index = 0; index < res.length; index++) {
+							//* por cada personaje llama a la funcion que devuelve el fragmento html a insertar
+							TITULO.innerHTML += await pintaTarjeta(res, index);
+						}
 					}
 				}
-			}
 
-			//* añade un escuchador (handleFuction) click por cada tarjeta
-			document.querySelectorAll(".card").forEach((tarjeta) => tarjeta.addEventListener("click",handleFuction));
-			
-			
-		}
-	}); 
+				//* añade un escuchador (handleFuction) click por cada tarjeta
+				document.querySelectorAll(".card").forEach((tarjeta) => tarjeta.addEventListener("click", handleFuction));
+			}
+		}); 
 }
 	
 //!========================================================================================!//
@@ -203,6 +213,5 @@ async function main(){
 //!                                    SCOPE PRINCIPAL                                     !//
 //!                                                                                        !//
 //!========================================================================================!//
-
 main();
 
